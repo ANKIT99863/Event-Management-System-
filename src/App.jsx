8051e47dd77cb9b80613e3bdf5ae8b4a68c3f94e
ANKIT99863 <ankitjaiswal.5074@gmail.com>
2024-05-26 23:20:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import EventManagement from './component/event.';
+import Addevent from './component/addevent';
+import Deleteevent from './component/deleteevent';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AdminHome from './component/AdminHome';
+import Register from './component/Register';
+import Joined from './component/joined';
+import Login from './component/login';
+import UserEvent from './Component/UserEvent';
+function App() {
+  
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/fetchevent" element={<EventManagement />} />
+          <Route path="/addevent" element={<Addevent />} />
+          <Route path="/deleteevent" element={<Deleteevent />} />
+          <Route path="/adminhome" element={<AdminHome/>} />
+          <Route path="/joined" element={<Joined/>} />
+          <Route path="/" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+            <Route path="/myevents" element={<UserEvent/> } />
+
+        </Routes>
+      </BrowserRouter>
+
+     {/* <Addevent/> */}
+
+      
+    </div>
+  );
+}
+
+export default App;
